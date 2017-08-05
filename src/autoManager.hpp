d765a930e027a92c7187c8b860b098e1f0ead10a
Estenoor <SamzFerg@gmail.com>
2017-08-05 11:03:25 -0500

    Removed Auto Variables from where I moved it into AutoManager.hpp and kept them in Robot.cpp
    Signed-off-by: Estenoor <SamzFerg@gmail.com>

--- src/autoManager.hpp
+++ src/autoManager.hpp
@@ -34,52 +34,6 @@ namespace FRC
 			void centerAuto();
 			void rightAuto();
 			void leftAuto();
-
-			//Auto Variable Declarations
-			int step = 1;
-
-					// ----------- Step 1 Vars -----------
-					double autoJoystick = -0.4;
-					double targetDistance = 2.75;
-					double encDistance = 0;
-
-					// ----------- Step 2 Vars -----------
-					bool angleStatus = false;
-					double prevAngle = -1;
-					double Angle = 0;
-					double targetAngle = 300; //Default: 360
-					int direction = 1;
-					double thresholdAngle2 = 3.0;
-					double rotateSpeed = 0.3;
-
-					// ----------- Step 3 Vars -----------
-					double pixelThreshold = 3;
-
-					// ----------- Step 5 Vars -----------
-					bool lidarStatus = false;
-					double lidarDistance = 0;
-					double startLimit = 1;
-					double endLimit = 0.4;
-					double controlLimit = 0;
-					double lidarMin = 1000000;
-
-					// ----------- Step 6 Vars -----------
-					int gearTarget = 20;
-					int gearCounter = 0;
-					int gearCounter2nd = 0;
-
-					// ----------- Step 7 Vars -----------
-					int reverseCounter = 0;
-					int reverseTarget = 30;
-
-					// ----------- Step 8 Vars -----------
-					double targetReturnAngle = 360.0;
-					double thresholdAngle8 = 10;
-
-					// ----------- Step 9 Vars -----------
-					int finalDriveCounter = 0;
-					int finalDriveCounterTarget = 350;
-
 	};
 }
 #endif /* SRC_AUTOMANAGER_HPP_ */
