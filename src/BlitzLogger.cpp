9c462676c0d166a2626f312e904c1c5710cf102f
Estenoor <SamzFerg@gmail.com>
2017-08-08 19:51:17 -0500

    Updated Logging Framework to include Timestamps in log messages and in the nameing of the log file
    Signed-off-by: Estenoor <SamzFerg@gmail.com>

--- src/BlitzLogger.cpp
+++ src/BlitzLogger.cpp
@@ -12,13 +12,16 @@ FRC::BlitzLogger::BlitzLogger()
 
 void FRC::BlitzLogger::init(bool debugMode)
 {
-	logfile.open("Placeholder.txt");
-	writeDebugMessage = debugMode;
+	char dateTime[] = std::chrono::system_clock::now();
+	logfile.open(dateTime);
+	writeDebugMessage = debugMode;;
 }
 
-void FRC::BlitzLogger::info(char message[])
+void FRC::BlitzLogger::info(char currentStage[], char message[])
 {
-	logfile << "[placeholder for Timestamp]"; //writes timestamp to file
+	logfile << "[" << std::chrono::system_clock::now() << "]"; //writes timestamp to file
+	logfile << " "; //Separating time stamp and message type
+	logfile << "[" << currentStage << "]"; //writes timestamp to file
 	logfile << " "; //Separating time stamp and message type
 	logfile << "[Info]"; //Message Type
 	logfile << " "; //Separating message type and message
@@ -26,11 +29,13 @@ void FRC::BlitzLogger::info(char message[])
 	logfile << "\n"; //Sets writing location to next line for next log message
 }
 
-void FRC::BlitzLogger::debug(char message[])
+void FRC::BlitzLogger::debug(char currentStage[], char message[])
 {
 	if(writeDebugMessage)
 	{
-		logfile << "[placeholder for Timestamp]"; //writes timestamp to file
+		logfile << "[" << std::chrono::system_clock::now() << "]"; //writes timestamp to file
+		logfile << " "; //Separating time stamp and message type
+		logfile << "[" << currentStage << "]"; //writes timestamp to file
 		logfile << " "; //Separating time stamp and message type
 		logfile << "[Debug]"; //Message Type
 		logfile << " "; //Separating message type and message
@@ -39,9 +44,26 @@ void FRC::BlitzLogger::debug(char message[])
 	}
 }
 
-void FRC::BlitzLogger::warning(char message[])
+void FRC::BlitzLogger::trace(char currentStage[], char message[])
 {
-	logfile << "[placeholder for Timestamp]"; //writes timestamp to file
+	if(writeDebugMessage)
+	{
+		logfile << "[" << std::chrono::system_clock::now() << "]"; //writes timestamp to file
+		logfile << " "; //Separating time stamp and message type
+		logfile << "[" << currentStage << "]"; //writes timestamp to file
+		logfile << " "; //Separating time stamp and message type
+		logfile << "[Trace]"; //Message Type
+		logfile << " "; //Separating message type and message
+		logfile << message; //Writes message to file
+		logfile << "\n"; //Sets writing location to next line for next log message
+	}
+}
+
+void FRC::BlitzLogger::warning(char currentStage[], char message[])
+{
+	logfile << "[" << std::chrono::system_clock::now() << "]"; //writes timestamp to file
+	logfile << " "; //Separating time stamp and message type
+	logfile << "[" << currentStage << "]"; //writes timestamp to file
 	logfile << " "; //Separating time stamp and message type
 	logfile << "[Warning]"; //Message Type
 	logfile << " "; //Separating message type and message
@@ -49,9 +71,11 @@ void FRC::BlitzLogger::warning(char message[])
 	logfile << "\n"; //Sets writing location to next line for next log message
 }
 
-void FRC::BlitzLogger::error(char message[])
+void FRC::BlitzLogger::error(char currentStage[], char message[])
 {
-	logfile << "[placeholder for Timestamp]"; //writes timestamp to file
+	logfile << "[" << std::chrono::system_clock::now() << "]"; //writes timestamp to file
+	logfile << " "; //Separating time stamp and message type
+	logfile << "[" << currentStage << "]"; //writes timestamp to file
 	logfile << " "; //Separating time stamp and message type
 	logfile << "[Error]"; //Message Type
 	logfile << " "; //Separating message type and message
